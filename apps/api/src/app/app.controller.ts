0c1d75a76b90d8eb8344edd315d5f52266e1c83e
erik <eabnelson@gmail.com>
2023-09-07 09:30:35 -0400

    rss feed builder

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Res, Req, Param } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { apiEnv } from '../environments/environment';
 import { JsonRpcProvider, ethers } from 'ethers';
+import * as xmlJs from 'xml-js';
 
 const Channel = require('../abi/Channel.json');
 const ChannelFactory = require('../abi/ChannelFactory.json');
@@ -36,9 +37,72 @@ export class AppController {
 		}
 	}
 
-	// Get details and posts from a given channel
-	@Get('channel/:channelAddress')
+	// Get RSS feed for a channel
+	@Get('channelFeed/:channelAddress')
 	async getChannelFeed(@Param('channelAddress') channelAddress: string, @Res() res: Response) {
+		try {
+			const channel = new ethers.Contract(channelAddress, Channel.abi, this.provider);
+
+			const channelDetails = await channel.queryChannel();
+
+			const [owner, title, symbol, description] = channelDetails[0];
+
+			// Create a structured Channel object
+			const channelData = {
+				owner,
+				title,
+				symbol,
+				description
+			};
+
+			const posts = channelDetails[1]
+				.map((item) => {
+					const [id, author, authorName, title, link, description, content, deleted] =
+						item;
+					return (
+						!deleted && {
+							title: title,
+							link: link,
+							description: description,
+							content: content,
+							author: authorName,
+							authorAddress: author
+						}
+					);
+				})
+				.filter(Boolean); // Filter out deleted posts or mapping returned falsy
+
+			const xmlObj = {
+				rss: {
+					_attributes: {
+						'xmlns:atom': 'http://www.w3.org/2005/Atom',
+						'xmlns:content': 'http://purl.org/rss/1.0/modules/content/',
+						version: '2.0'
+					},
+					channel: {
+						ownerAddress: owner,
+						title: title,
+						symbol: symbol,
+						description: description,
+						item: posts
+					}
+				}
+			};
+
+			const xml = xmlJs.js2xml(xmlObj, { compact: true, spaces: 4 });
+
+			res.set('Content-Type', 'application/xml');
+			return res.send(xml);
+		} catch (error) {
+			return res
+				.status(500)
+				.json({ message: 'Failed to get channel details', error: error.message });
+		}
+	}
+
+	// Get channel details in JSON format
+	@Get('channelDetails/:channelAddress')
+	async getChannelDetails(@Param('channelAddress') channelAddress: string, @Res() res: Response) {
 		try {
 			const channel = new ethers.Contract(channelAddress, Channel.abi, this.provider);
 
@@ -59,18 +123,18 @@ export class AppController {
 				.map((item) => {
 					const [id, author, authorName, title, link, description, content, deleted] =
 						item;
-					return {
-						id: id.toString(), // Convert the postId from BigInt to string
-						author,
-						authorName,
-						title,
-						link,
-						description,
-						content,
-						deleted
-					};
+					return (
+						!deleted && {
+							title,
+							link,
+							description,
+							content,
+							author: authorName,
+							authorAddress: author
+						}
+					);
 				})
-				.filter((post) => !post.deleted); // Filter out the posts where deleted is true
+				.filter(Boolean); // Filter out deleted posts or mapping returned falsy
 
 			return res.json({ channel: channelData, posts: posts });
 		} catch (error) {
