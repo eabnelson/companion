db572e02cecea2320787b3add05e71db933cf016
erik <eabnelson@gmail.com>
2023-09-07 07:02:22 -0400

    testnet addresses

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -8,6 +8,9 @@ const { api } = apiEnv;
 export class AppController {
 	constructor() {}
 
+	// ChannelFactory getChannels to get list of channel addresses
+	// ChannelFactory address: 0x0118d7720B92A29F6653572bd95555c07CAed4CA
+	// Channel address: 0x82E6075D7cd3555264b26b1a11a41B0625Bf7Bc7
 	@Get('channels')
 	async getEpisodeContent(@Req() request: Request, @Res() res: Response) {}
 }
